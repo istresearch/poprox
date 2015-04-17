06a711cbefe09daa75ca755595db0cc4ca190ac5
Ryan Fischbach <ryan.fischbach@istresearch.com>
2015-04-17 11:55:47 -0400

    Update to framework v5 (3.1.2) - Website resource classes should have a BitsWebsite ultimate ancestor so that many of the non-translated values/methods can be placed in /res rather than /res/i18n/en|es|de|etc.

--- poprox/res/i18n/en/Website.php
+++ poprox/res/i18n/en/Website.php
@@ -1,24 +1,15 @@
 <?php
 namespace BitsTheater\res\en;
 use BitsTheater\res\Website as BaseResources;
-use com\blackmoonit\Arrays;
 {//begin namespace
 
 class Website extends BaseResources {
-	public $feature_id = 'IST Research: Poprox website';
-	public $version_seq = 2;	//build number, inc if db models need updating
-	public $version = '2.3.3';	//displayed version text
 	
-	public $header_meta_title = 'Roxy';
 	public $header_title = 'Roxy';
 	public $header_subtitle = 'Turn on the spotlight.';
 	
-	public $list_patrons_html = array(
-			'prime_investor' => '<a href="http://www.istresearch.com/">IST Research, LLC.</a>',
-	);
-	
-	//public $list_credits_html_more = array(
-	//);
+	public $menu_home_label = 'Home';
+	public $menu_home_subtext = '';
 	
 	/**
 	 * Some resources need to be initialized by running code rather than a static definition.
@@ -27,48 +18,8 @@ class Website extends BaseResources {
 	public function setup($aDirector) {
 		parent::setup($aDirector);
 		
-		//we do not want the following libs universally loaded, case by case only
-		//Bootstrap
-		unset($this->css_load_list['bootstrap/css/bootstrap.css']);
-		Arrays::removeValue($this->js_libs_load_list, 'bootstrap/js/bootstrap.min.js');
-		//Bootbox
-		Arrays::removeValue($this->js_libs_load_list, 'bootbox/bootbox.js');
-
-		//NULL path means use default lib path
-		$this->res_array_merge($this->js_load_list, array(
-				//minification from http://www.jsmini.com/ using Basic and no jquery included.
-				'webapp_mini.js' => WEBAPP_JS_URL,
-				//  !-remove the below space and comment out the above line to debug un-minified JS code
-				/* * /
-				'webapp.js' => WEBAPP_JS_URL,
-				'BitsRightGroups.js' => WEBAPP_JS_URL,
-				//'AnotherFile.js' => WEBAPP_JS_URL,
-				/* end of webapp JS */
-		));
-
-		//NULL path means use default lib path
-		$this->res_array_merge($this->css_load_list, array(
-				'roxy.css' => BITS_RES.'/style',
-		));
-
-		$this->res_array_merge($this->list_credits_html, array(
-				'roxy_logo' => '<a target="_blank" href="http://www.behance.net/gallery/Human-TraffickingAwareness-Posters/10072737">Logo</a> non-com (c) by <a target="_blank" href="http://paodj.prosite.com/163867/resume">Paola</a> <a target="_blank" href="mailto:me@paodiaz.com">Diaz</a>',
-		));
-	}
-	
-	/**
-	 * Override this function if your website needs to do some updates that are not database related.
-	 * Throw an exception if your update did not succeed.
-	 * @param number $aSeqNum - the version sequence number (<= what is defined in your overriden Website class).
-	 * @throws Exception on failure.
-	 */
-	public function updateVersion($aSeqNum) {
-		try {
-			//nothing to do, yet
-		} catch (Exception $e) {
-			//throw expection if your update code fails (logging it would be a good idea, too).
-			$this->debugLog(__METHOD__.' '.$e->getMessage());
-			throw $e;
+		if (empty($this->header_meta_title)) {
+			$this->header_meta_title = 'Roxy';
 		}
 	}
 
